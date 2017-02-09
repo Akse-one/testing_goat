c8b5b558efc06f025a6d4ca53f08e503d1edc44c
Aaron <aaron.chandler@mediagamma.com>
2017-02-09 12:51:27 +0000

    T2: adding unit test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,42 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000/')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+	def setUp(self):
+	    self.browser = webdriver.Firefox()
+	    self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+	    self.browser.quit()
+
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+            #user hears about an online to-do app
+            #user goes to check the homepage
+            self.browser.get('http://127.0.0.1:8000/')
+
+            #User notices the  page title and header mention To-do lists
+            self.assertIn('To-Do', self.browser.title)
+            self.fail('Finish the test!')
+
+	    #User is invited to enter a to-do item straight away
+
+	    #User types in a to-do item
+
+	    #User hits enter and the page updates and  now lists the todo item
+
+	    #There is stull a text box inviting user to add another to-do item
+
+	    #The page updates again and now displays 2 to-do items
+
+	    #User wonders if the site will remember their lists, the user sees that there is a unique url 
+	    #for the list
+            #There is also some explaination text to that effect
+
+            #User visits url. The to-do list is there
+
+            #The user is satisfied
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
