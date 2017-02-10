860d44f168d6cc5c1c2b1be36969fab0f15ed436
Aaron <aaron.chandler@mediagamma.com>
2017-02-10 12:56:33 +0000

    T10: making the test better

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 # User hears about an online to-do app
 # User goes to check the homepage
@@ -34,17 +39,15 @@ class NewVisitorTest(unittest.TestCase):
 # User hits enter and the page updates and  now lists the todo item
 
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Finish the obey_goat book', [row.text for row in rows])
-        self.assertIn(
-            '2: Use book to make dollah' ,
-             [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Finish the obey_goat book ')
 
 # There is still a text box inviting user to add another to-do item
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use book to make dollah')
+        inputbox.send_keys(Keys.Enter)
 # The page updates again and now displays 2 to-do items
+        self.check_for_row_in_list_table('1: Finish the obay_goat book')
+        self.check_for_row_in_list_table('2: Use book to make dollah')
 # User wonders if the site will remember their lists, the user sees that there is a unique url
 # for the list
 # There is also some explaination text to that effect
